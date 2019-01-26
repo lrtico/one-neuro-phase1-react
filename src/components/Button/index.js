8c34fe7bce41f21f1b8ac6b680a13072b3aa7ebb
Jackson Powell <jacksonpowell@Jacksons-MacBook-Pro-Touchbar.local>
2019-01-26 14:52:13 -0800

    Send data to API and download a PDF with a CSS loader to indicate status

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,10 +4,10 @@ import "./buttonStyles.css";
 import MaterialIcon from "react-google-material-icons";
 
 const Button = props => {
-  const { buttonLabel } = props;
+  const { buttonLabel, cssClasses } = props;
   return (
     <div className="btn-wrap flex--center-vertical">
-      <div className="btn">
+      <div className={cssClasses === undefined ? "btn" : `btn ${cssClasses}`}>
         <div className="btn__hover" />
         <button className="btn__text flex--space-between">
           <span>{buttonLabel}</span>
--- src/pages/WizardForm50Page.js
+++ src/pages/WizardForm50Page.js
@@ -3,31 +3,77 @@ import { reduxForm, getFormValues } from "redux-form";
 import { connect } from "react-redux";
 import FormAPI from "formapi";
 import validate from "../validate";
+import "../app.css";
 import Button from "../components/Button";
 import Appendix from "../components/Test/Appendix";
 
 class WizardForm50Page extends Component {
+  state = {
+    loading: false
+  };
+
   generatePDFTest = event => {
     event.preventDefault();
     const values = this.props.values;
-    console.log("Make submission go now!", values);
+
+    this.setState({ loading: true });
+
     const formapiConfig = new FormAPI.Configuration();
     formapiConfig.apiTokenId = "api_test_nK62yQ9snkS4CQqJYA";
     formapiConfig.apiTokenSecret =
       "xO7QZdhq_CqdfJHdPx8wWqwZrwrncNO2I5LNvOZe6Pg";
     const formapiClient = new FormAPI.Client(formapiConfig);
-
     const TEMPLATE_ID = "tpl_kbrXFcG2zFCHQTyrSQ";
-
     const OPTIONS = {
       data: values,
       test: true
     };
 
     formapiClient.generatePDF(TEMPLATE_ID, OPTIONS, function(error, response) {
-      if (error) throw error;
+      let downloadURL = response.submission.download_url;
+      let status = response.submission.state;
       console.log("FormAPI response", response);
+      console.log("Make download go now!", downloadURL);
+
+      if (error) throw error;
+
+      status !== "processed"
+        ? console.log("API is processing")
+        : downloadPDF(downloadURL);
     });
+
+    const downloadPDF = url => {
+      console.log("API is done");
+      window.open(url);
+      this.setState({ loading: false });
+    };
+
+    // const URL =
+    //   "https://api.formapi.io/api/v1/templates/tpl_kbrXFcG2zFCHQTyrSQ/submissions";
+    // const TOKENID = "api_test_nK62yQ9snkS4CQqJYA";
+    // const TOKENSECRET = "xO7QZdhq_CqdfJHdPx8wWqwZrwrncNO2I5LNvOZe6Pg";
+    // headers.set('Authorization', 'Basic ' + Buffer.from(TOKENID + ":" + TOKENSECRET).toString('base64'))
+    // let headers = new Headers();
+    // headers.append(
+    //   "Authorization",
+    //   "Basic " + window.btoa(TOKENID + ":" + TOKENSECRET)
+    // );
+    // fetch(URL, {
+    //   method: "POST", // or 'PUT'
+    //   body: JSON.stringify(OPTIONS), // data can be `string` or {object}!
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //     Authorization: "Basic " + window.btoa(TOKENID + ":" + TOKENSECRET)
+    //   }
+    // })
+    //   .then(response => {
+    //     return response.json();
+    //   })
+    //   .then(jsonResponse => {
+    //     console.log(jsonResponse);
+    //   })
+    //   .catch(error => console.error("Error:", error));
   };
 
   render() {
@@ -37,7 +83,23 @@ class WizardForm50Page extends Component {
     return (
       <form className="col" onSubmit={this.generatePDFTest}>
         <Appendix values={this.props} />
-        <Button onClick={this.generatePDFTest} buttonLabel="OK" />
+        <div className="flex flex--center-vertical">
+          <Button
+            onClick={this.generatePDFTest}
+            buttonLabel="Make test PDF"
+            cssClasses="btn--width-auto"
+          />
+          {this.state.loading ? (
+            <div className="pdf-generation-loader">
+              <div className="pdf-generation-loader__box">
+                <div />
+                <div />
+                <div />
+              </div>
+              <div className="pdf-generation-loader__arrow" />
+            </div>
+          ) : null}
+        </div>
       </form>
     );
   }
