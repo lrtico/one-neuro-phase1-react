9d7f60cfd9675c3567a6a5e5085f6457aec8f8ce
Jackson Powell <jacksonpowell@Jacksons-MacBook-Pro-Touchbar.local>
2019-02-21 21:27:39 -0800

    Fixed DescriptionCheckboxCard component

--- src/components/Card/DescriptionCheckboxCard/DescriptionCheckboxCard.js
+++ src/components/Card/DescriptionCheckboxCard/DescriptionCheckboxCard.js
@@ -4,21 +4,29 @@ import validate from "../../../validate";
 import "./descriptionCheckboxCardStyles.css";
 import "../../../app.css";
 import "../../Card/cardStyles.css";
-import { randomId } from "../../../utils/Helpers";
 import MaterialIcon from "react-google-material-icons";
 
-function handleTick(event) {
-  const tar = event.currentTarget.parentNode;
-  tar.classList.toggle("question__checkbox--selected");
-  console.log("hanleTick fired, ", event);
-}
+// function handleTick(event) {
+//   const tar = event.currentTarget.parentNode;
+//   tar.classList.toggle("question__checkbox--selected");
+//   console.log("hanleTick fired, ", event);
+// }
 
 const handleTextboxTick = (event, name, props) => {
   const tar = event.currentTarget;
   tar.parentNode.parentNode.classList.add("question__checkbox--selected");
-  console.log("handleTextboxTick's props = ", event);
+  //console.log("handleTextboxTick's input value = ", tar.value);
   props.change(name, true);
-}
+};
+
+const handleTextBoxClear = event => {
+  const tar = event.currentTarget;
+  const val = tar.value;
+  //console.log("onBlur fired", tar, val);
+  if (val === "") {
+    tar.parentNode.parentNode.classList.remove("question__checkbox--selected");
+  }
+};
 
 const DescriptionCheckboxCard = ({
   label,
@@ -29,8 +37,7 @@ const DescriptionCheckboxCard = ({
   liClasses,
   cardName,
   cardNameDescription,
-  ...props,
-  meta: { touched, error }
+  ...props
 }) => {
   console.log("DescriptionCheckboxCard props = ", props);
   return (
@@ -47,7 +54,7 @@ const DescriptionCheckboxCard = ({
           <div className="question__choice--radio-shield" />
           {checkboxInfo.map(card => (
             <li
-              key={randomId()}
+              key={card.cardNameCheckbox}
               className={
                 card.liClasses == null
                   ? "question__choice"
@@ -56,7 +63,7 @@ const DescriptionCheckboxCard = ({
               //tabIndex={card.tabOrder}
               // onClick={handleTick}
             >
-              <div className="desc-checkbox__checkbox question__text-wrap flex flex--center-vertical" onClick={handleTick}>
+              <div className="desc-checkbox__checkbox question__text-wrap flex flex--center-vertical">
                 <div className="question__label">
                   <div className="question__letter">
                     <span>{card.cardKey}</span>
@@ -69,12 +76,11 @@ const DescriptionCheckboxCard = ({
                   component="input"
                   className="desc-checkbox__checkbox__input"
                 />
+                <span className="question__tick--bg" />
+                <span className="question__tick" />
                 <div className="question__bg desc-checkbox__bg" />
               </div>
 
-              <div className="question__tick-wrap">
-                <MaterialIcon icon="check" />
-              </div>
               {card.thumbnail === undefined ? null : (
                 <div className="question__image-wrap">
                   <img src={card.thumbnail} alt={card.thumbnailAlt} />
@@ -87,23 +93,27 @@ const DescriptionCheckboxCard = ({
                   component="input"
                   type="text"
                   className="desc-checkbox__desc__input"
-                  onKeyPress={(event) => handleTextboxTick(event, card.cardNameCheckbox, props)}
-                  //onKeyPress={() => console.log("text input key pressed")}
+                  onKeyUp={event =>
+                    handleTextboxTick(event, card.cardNameCheckbox, props)
+                  }
+                  onBlur={event => handleTextBoxClear(event)}
                 />
+                <div className="question__tick-wrap">
+                  <MaterialIcon icon="check" />
+                </div>
                 <div className="question__bg" />
               </div>
             </li>
           ))}
-          {touched && error && <span>{error}</span>}
         </ul>
       </div>
     </div>
   );
 };
 export default reduxForm({
-  form: 'wizard', // <------ same form name
+  form: "wizard", // <------ same form name
   destroyOnUnmount: false, // <------ preserve form data
   forceUnregisterOnUnmount: true, // <------ unregister fields on unmount
   validate
-})(DescriptionCheckboxCard)
+})(DescriptionCheckboxCard);
 //export default DescriptionCheckboxCard;
