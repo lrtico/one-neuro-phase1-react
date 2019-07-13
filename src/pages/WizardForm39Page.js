7a71129f9374d3cd278c247b453547dcb6423a2c
Jackson Powell <jacksonpowell@Jacksons-MacBook-Pro-Touchbar.local>
2019-07-13 15:39:40 -0700

    Removed hypens from Field names in Wizard Page 39 because they aren't allowed in the PDF template

--- src/pages/WizardForm39Page.js
+++ src/pages/WizardForm39Page.js
@@ -21,73 +21,73 @@ const WizardForm39Page = props => {
         component={CheckboxCard}
         checkboxInfo={[
           {
-            cardName: "bt-overstimulated",
+            cardName: "btOverstimulated",
             cardKey: "A",
             cardLabel: "Is easily overstimulated in play",
             tabOrder: "1"
           },
           {
-            cardName: "bt-attention-span",
+            cardName: "btAttentionSpan",
             cardKey: "B",
             cardLabel: "Has a short attention span",
             tabOrder: "2"
           },
           {
-            cardName: "bt-self-control",
+            cardName: "btSelfControl",
             cardKey: "C",
             cardLabel: "Lacks self-control",
             tabOrder: "3"
           },
           {
-            cardName: "bt-unhappy",
+            cardName: "btUnhappy",
             cardKey: "D",
             cardLabel: "Seems unhappy most of the time",
             tabOrder: "4"
           },
           {
-            cardName: "bt-affectionless",
+            cardName: "btAffectionless",
             cardKey: "E",
             cardLabel: "Withholds affection",
             tabOrder: "5"
           },
           {
-            cardName: "bt-hides-feelings",
+            cardName: "btHidesFeelings",
             cardKey: "F",
             cardLabel: "Hides feelings",
             tabOrder: "6"
           },
           {
-            cardName: "bt-over-engergetic",
+            cardName: "btOverEngergetic",
             cardKey: "G",
             cardLabel: "Seems over energetic in play",
             tabOrder: "7"
           },
           {
-            cardName: "bt-impulsive",
+            cardName: "btImpulsive",
             cardKey: "H",
             cardLabel: "Seems impulsive",
             tabOrder: "8"
           },
           {
-            cardName: "bt-overreacts-problems",
+            cardName: "btOverreactsProblems",
             cardKey: "I",
             cardLabel: "Overreacts in the face of problems",
             tabOrder: "9"
           },
           {
-            cardName: "bt-shy",
+            cardName: "btShy",
             cardKey: "J",
             cardLabel: "Uncomfortable meeting new people",
             tabOrder: "10"
           },
           {
-            cardName: "bt-parental-attention",
+            cardName: "btParentalAttention",
             cardKey: "K",
             cardLabel: "Requires a lot of parental attention",
             tabOrder: "11"
           },
           {
-            cardName: "bt-cannot-calm",
+            cardName: "btCannotCalm",
             cardKey: "L",
             cardLabel: "Cannot calm down",
             tabOrder: "12"
@@ -96,7 +96,7 @@ const WizardForm39Page = props => {
         label="Does this child"
         labelBold="exhibit"
         labelLast="any of the following"
-        name="bt-exhibit-group"
+        name="btExhibitGroup"
         classes="question question--thumbless"
         disabled={disabled}
       />
@@ -106,7 +106,7 @@ const WizardForm39Page = props => {
         label="What"
         labelBold="fears"
         labelLast="does this child have, if any"
-        name="bt-fears"
+        name="btFears"
         src="img/icons-ghost.svg"
         tabOrder="13"
         type="input"
@@ -118,7 +118,7 @@ const WizardForm39Page = props => {
         component={TextQuestion}
         label="What makes this child"
         labelBold="angry"
-        name="bt-angry"
+        name="btAngry"
         src="img/icons-bomb.svg"
         tabOrder="14"
         type="input"
